15b5d52b2d70f0d01d274d2c500db153c020ec10
Sydney Grant <sg3957@nyu.edu>
2018-09-29 19:30:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,11 +9,11 @@ end
 
 def reverse_each_word(words)
 #   # words.split(" ")
-  # new_words = []
+  new_words = []
   words.split.collect do |x| 
-    x.reverse!
+    new_words << x.reverse!
     
   end
-  
+  new_words.join(" ")
 end
 
