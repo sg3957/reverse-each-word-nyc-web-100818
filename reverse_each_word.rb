4d20721551e8928eeee54573937ad80ca5d3128c
Sydney Grant <sg3957@nyu.edu>
2018-09-29 16:08:29 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,25 @@
+def reverse_each_word(sentence)
+  new_string = []
+  sentence.split.each do |x|
+    new_string << x.reverse!
+  end
+  new_string.join(" ")
+end
+
+
+def reverse_each_word(words)
+  # words.split(" ")
+  words.split.collect do |x| 
+    x.reverse!
+  end
+  
+end
+
+# def reverse_each_word(sentence)
+
+#   new_string = ""
+#   sentence.split.each do |x|
+#     new_string << x.reverse! + " "
+#   end
+#   new_string
+# end
