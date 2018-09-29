89be27c912cf522944b152ce5f5be74e39198276
Sydney Grant <sg3957@nyu.edu>
2018-09-29 19:30:16 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,18 +8,12 @@ end
 
 
 def reverse_each_word(words)
-  # words.split(" ")
+#   # words.split(" ")
+  new_words = []
   words.split.collect do |x| 
-    x.reverse!
+    new_words << x.reverse!
+    
   end
-  
+  new_words.join(" ")
 end
 
-# def reverse_each_word(sentence)
-
-#   new_string = ""
-#   sentence.split.each do |x|
-#     new_string << x.reverse! + " "
-#   end
-#   new_string
-# end
